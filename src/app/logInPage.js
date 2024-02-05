3c1e111b072bfae9157eba7cbadee0feada6073e
Sanju2056 <sanjugurung1999@gmail.com>
2024-02-05 23:30:44 +0545

    login Page modified

--- src/app/logInPage.js
+++ src/app/logInPage.js
@@ -1,17 +1,26 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
 import Image from 'next/image'
+
+
 
 const LogInPage = () => {
     const footerData = ["Meta","About","Blog","Jobs","Help","API","Privacy","Terms","Locations","Instagram Lite"
     ,"Threads","Contact Uploading & Non-Users","Meta Verified"]
     const Language=["English","French","Japanese","Korean","Spanish","Russian","Italian","German","Hindi",
         "Netherlands"]
+        const[userName,setUserName]= useState('')
+        const[password,setPassword]= useState('')
+        const HandleForm = () =>{
+            console.log(userName,password)
+        }
     return (
         <div className=' h-[100vh] flex flex-col  items-center'>
             <div className='h-[400px] w-[330px] border p-10 mt-4 flex flex-col items-center'>
                 <div className='h-[90px] w-[100%] flex items-center justify-center'>
                     <Image
                         src='/image/download.png'
+                        alt='Instagram Logo'
                         height={90}
                         width={330}
                         className='h-[90px] w-[90%]'
@@ -22,15 +31,19 @@ const LogInPage = () => {
                         placeholder='Phone number, username or email'
                         className='h-[40px] w-[100%]  text-[11px] pl-2 
                         border rounded outline-none bg-[#fafafa] mb-2'
+                        onChange={(e)=>setUserName(e.target.value)} 
                     />
                     <input
                         placeholder='Password'
+                        type='password'
                         className='h-[40px] w-[100%] text-[11px] pl-2 
                         border rounded outline-none bg-[#fafafa] mb-2'
+                        onChange={(e)=>setPassword(e.target.value)} 
                     />
                     <div className='h-[35px] w-[100%] text-[11px] pl-2 border 
                         rounded-lg outline-none bg-[#4cb5f9] flex justify-center
-                         items-center my-2 cursor-pointer'>
+                         items-center my-2 cursor-pointer'
+                         onClick={()=>{HandleForm()}}>
                         <p className=' text-[#fff] text-[13px] font-semibold'>Log in</p>
                     </div>
                 </form>
@@ -42,6 +55,7 @@ const LogInPage = () => {
                 <div className='flex justify-center items-center cursor-pointer'>
                     <Image
                         src='/image/facebook-icon.png'
+                        alt='facebook icon'
                         width={1}
                         height={1}
                         className='h-[15px] w-[15px]'
@@ -58,12 +72,14 @@ const LogInPage = () => {
             <div className='h-[60px] w-[330px] flex items-center justify-center  gap-2'>
             <Image
                 src='/image/google-play.png'
+                alt='google-play icon'
                 width={120}
                 height={10}  
                 className='h-[40px]'
             />
             <Image
                 src='/image/microsoft.png'
+                alt='microsoft icon'
                 width={120}
                 height={10}  
                 className='h-[40px]'
@@ -72,17 +88,17 @@ const LogInPage = () => {
             <div className='h-[30px] w-[65%] flex gap-4 items-center
              justify-center mt-6 text-[#918f8f]'>
                 {
-                    footerData.map((item)=>{
-                       return <p className='text-[11px] cursor-pointer'>{item}</p>
+                    footerData.map((item,i)=>{
+                       return <p className='text-[11px] cursor-pointer' key={i}>{item}</p>
                     })
                 }
             </div>
             <div className='h-[50px] w-[max-content] flex justify-center items-center'>
                 <select className='w-[max-content] text-[12px] text-[#918f8f]'>
                     { 
-                        Language.map((language)=>{
+                        Language.map((language,i)=>{
                             return <option className='w-[max-content] border-none
-                             appearance-none '>{language}</option>
+                             appearance-none' key={i}>{language}</option>
                         })
                     }
                 </select>
